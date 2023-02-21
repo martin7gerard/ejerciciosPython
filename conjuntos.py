730b32c668fef37fbb65f73cab414c242154135d
Martin Casillas <mt7g@hotmail.com>
2023-02-20 20:27:49 -0600

    Update conjuntos.py

--- conjuntos.py
+++ conjuntos.py
@@ -1,3 +1,4 @@
+# Por Martin Gerardo Casillas Sánchez
 # 1 Preguntar los elementos de 2 conjuntos
 datos = input("Dame los elementos del conjunto A separados por comas: ") # 1,2,3,4,5,6
 lista = datos.split(",") # Split -> Separar
@@ -9,16 +10,16 @@ conjuntoB = set(lista2) # creamos el conjunto set()
 
 # 2 Mostrar relaciones de igualdad
 if conjuntoA == conjuntoB: # condicion
-	print("Los conjuntos A y B son iguales")
+  print("Los conjuntos A y B son iguales")
 else:
-	print("Los conjuntos A y B NO son iguales")
+  print("Los conjuntos A y B NO son iguales")
 
 # 3 Relación de subconjuntos
 if conjuntoA.issubset(conjuntoB):
-	print("El conjunto A es subconjunto de B")
+  print("El conjunto A es subconjunto de B")
 
 if conjuntoB.issubset(conjuntoA):
-	print("El conjunto B es subconjunto de A")
+  print("El conjunto B es subconjunto de A")
 
 # 4 Realizar las operaciones (AUB) (AnB) (A-B) (B-A)
 print("La operación AUB =", (conjuntoA | conjuntoB))
@@ -30,7 +31,7 @@ print("La operacion B-A =", (conjuntoB - conjuntoA))
 elemento = input("Dame un elemento y lo busco en los conjuntos: ")
 
 if elemento in conjuntoA:
-	print("El elemento", elemento, "se encuentra dentro del conjunto A")
+  print("El elemento", elemento, "se encuentra dentro del conjunto A")
 
 if elemento in conjuntoB:
-	print("El elemento", elemento, "se encuentra dentro del conjunto B")
+  print("El elemento", elemento, "se encuentra dentro del conjunto B")
